ae75cd073b0c5ad2d1412d0c7e2c9afcc84b55a9
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-07-19 20:55:45 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+def dfs(prev):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(prev, n+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs(i)
+        s.pop()
+        visited[i] = False
+    
+    
+n, m = map(int, input().split())
+s = []
+visited = [False]*(n+1)
+dfs(1)
