dc101bb7c0869f482946e12c5df8e32a4ca023a4
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-07-20 19:14:37 +0900

    [Silver II] Title: N과 M (12), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+def dfs():
+    if len(s) == m:
+        if tuple(s) not in result:
+            result[tuple(s)] = 1
+            print(*s)
+        return
+    for i in nums:
+        if len(s) == 0:
+            s.append(i)
+            dfs()
+            s.pop()
+        elif i >= s[len(s)-1]:
+            s.append(i)
+            dfs()
+            s.pop()
+            
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+s = []
+result = {}
+
+dfs()
