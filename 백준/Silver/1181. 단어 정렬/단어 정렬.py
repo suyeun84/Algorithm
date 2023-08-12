4114a033b2c5545ed646da908b14f2fef09e447f
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-08-12 22:38:36 +0900

    [Silver V] Title: 단어 정렬, Time: 88 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+word = []
+for i in range(n):
+    word.append(sys.stdin.readline().strip())
+
+word = list(set(word))
+word.sort()
+word.sort(key=len)
+for i in word:
+    print(i)
