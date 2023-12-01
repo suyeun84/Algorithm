5e7b4d38a3da2a596ff1a893e484a218f525c772
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-12-01 17:44:44 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.40 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    if(n == 1): return 1
+    if(n == 2): return 2
+    answer = [0]*(n+1)
+    answer[1] = 1
+    answer[2] = 2
+    for i in range(3,n+1):
+        answer[i] = answer[i-1] + answer[i-2]
+    return answer[n] % 1234567
