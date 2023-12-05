c8475007474fcad65ba04d6c15f5c05fefce1f8d
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-12-05 18:39:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,14 @@
-from collections import deque
+def check(s):
+    while True:
+        if '()' not in s and '{}' not in s and '[]' not in s:
+            break
+        s = s.replace("()", "").replace("{}", "").replace("[]","")
+    return 1 if len(s) == 0 else 0
+
 def solution(s):
     answer = 0
-    news = s + s
-    for i in range(0, len(s)):
-        tc = news[i: i+len(s)]
-        st = deque()
-        flag = True
-        for a in tc:
-            if a == "[" or a == "{" or a == "(":
-                st.append(a)
-            elif a == ")":
-                if len(st)==0 or st.pop() != "(":
-                    flag = False
-                    continue
-            elif a == "}":
-                if len(st)==0 or st.pop() != "{":
-                    flag = False
-                    continue
-            elif a == "]":
-                if len(st)==0 or st.pop() != "[":
-                    flag = False
-                    continue
-        if(flag == True and len(st)==0):
-            answer += 1
+    
+    for i in range(len(s)):
+        answer += check(s)
+        s = s[1:] + s[0]
     return answer
