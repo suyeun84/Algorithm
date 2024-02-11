128dae04932998aeacb48ab24cb02ded3a8b2215
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-02-12 01:32:33 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,20 +1,20 @@
 import heapq
 def solution(jobs):
+    answer, curr, fin_job = 0, 0, 0
     start = -1
-    answer, fin_job, curr = 0, 0, 0
-    waiting = []
+    heap = []
     jobs.sort()
     while fin_job < len(jobs):
         for job in jobs:
             if start < job[0] <= curr:
-                heapq.heappush(waiting, (job[1], job[0])) #(소요시간, 시작시간)
-        if len(waiting) == 0:
+                heapq.heappush(heap, (job[1], job[0])) #(시작시간, 소요시간)
+        if len(heap) == 0:
             curr += 1
             continue
-        job = heapq.heappop(waiting)
+        job = heapq.heappop(heap) #(소요시간, 시작시간)
         start = curr
-        curr += job[0]
-        answer += (curr - job[1])
+        curr = curr + job[0]
         fin_job += 1
-        
+        answer += (curr-job[1])
+            
     return answer // len(jobs)
