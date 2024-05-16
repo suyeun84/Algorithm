9e1999c7ee1ea6ed38962ebbf222a12ea284260e
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-05-16 17:24:18 +0900

    [Gold IV] Title: 연구소, Time: 2448 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,31 +1,65 @@
-from sys import stdin
+import sys
 from collections import deque
-from copy import deepcopy
 from itertools import combinations
-
-def bfs(graph):
-    q = deque([(j, i) for i in range(N) for j in range(M) if graph[i][j] == 2])
-    while q:
-        x, y = q.popleft()
-        for nx, ny in zip([x+1, x-1, x, x], [y, y, y+1, y-1]):
-            if 0 <= nx < M and 0 <= ny < N and not graph[ny][nx]:
-                graph[ny][nx] = 2
-                q.append((nx, ny))
-    
-    global answer
-    count = sum([i.count(0) for i in graph])
-    answer = max(answer, count)
+import copy
 
 
-N, M = map(int, stdin.readline().split())
-graph = [list(map(int, stdin.readline().split())) for _ in range(N)]
-x_y = [(x, y) for x in range(M) for y in range(N) if not graph[y][x]]
-answer = 0
+def solution():
+    input = sys.stdin.readline
+    N, M = map(int, input().split())
+    virus = []
+    d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    answer = 0
 
-for c in combinations(x_y, 3):
-    tmp_graph = deepcopy(graph)
-    for x, y in c:
-        tmp_graph[y][x] = 1
-    bfs(tmp_graph)
+    for _ in range(N):
+        virus.append(list(map(int, input().strip().split())))
 
-print(answer)
+    def bfs(tmp_virus):
+        nonlocal answer
+        q = deque()
+
+        for i in range(N):
+            for j in range(M):
+                if tmp_virus[i][j] == 2:
+                    q.append((i, j))
+
+        while q:
+            y, x = q.popleft()
+            for dy, dx in d:
+                ny = y + dy
+                nx = x + dx
+                if 0 > ny or ny >= N or nx < 0 or nx >= M:
+                    continue
+                if tmp_virus[ny][nx] == 0:
+                    tmp_virus[ny][nx] = 2
+                    q.append((ny, nx))
+
+        cnt = 0
+        for i in range(N):
+            for j in range(M):
+                if tmp_virus[i][j] == 0:
+                    cnt += 1
+        answer = max(answer, cnt)
+
+    def make_wall():
+        com = []
+        for i in range(N):
+            for j in range(M):
+                com.append([i,j])
+        for li in combinations(com, 3):
+            tmp_virus = copy.deepcopy(virus)
+            flag = True
+            for l in li:
+                if tmp_virus[l[0]][l[1]] == 0:
+                    tmp_virus[l[0]][l[1]] = 1
+                else:
+                    flag = False
+                    break
+            if flag:
+                bfs(tmp_virus)
+    make_wall()
+    print(answer)
+
+
+if __name__ == '__main__':
+    solution()
