453e12cd0c2052d4a10c5525020daec9e8072edd
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-06-03 15:28:39 +0900

    [Gold IV] Title: 최단경로, Time: 604 ms, Memory: 68244 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,41 +1,44 @@
 import sys
 import heapq
-input = sys.stdin.readline
-INF = int(1e9)
 
-# V=정점 개수, E=간선개수
-V, E = map(int, input().split())
-# K=시작 정점 번호
-K = int(input())
+def solution():
+    input = sys.stdin.readline
+    V, E = map(int, input().split())
+    K = int(input())
+    INF = int(1e9)
+    graph = [[] for _ in range(V+1)]
+    distance = [INF for _ in range(V+1)]
 
-graph = [[] * (V+1) for _ in range(V+1)]
-distance = [INF]*(V+1)
+    for _ in range(E):
+        u1, v1, w1 = map(int, input().split())
+        graph[u1].append((w1, v1))
 
-for _ in range(E):
-    # u -> v 가중치w
-    u, v, w = map(int, input().split())
-    #graph[u][v] = min(graph[u-1][v-1], w)
-    graph[u].append((v, w))
+    def dijkstra(start):
+        nonlocal distance
+        distance[start] = 0
+        heap = []
+        heapq.heappush(heap, (0, start))
+        while heap:
+            curr_distance, node = heapq.heappop(heap)
+            if curr_distance > distance[node]:
+                continue
+            for dist, end in graph[node]:
+                if curr_distance + dist >= distance[end]:
+                    continue
+                distance[end] = curr_distance + dist
+                heapq.heappush(heap, (distance[end], end))
 
-def dijkstra(start):
-    heap = []
-    heapq.heappush(heap, (0, start))
-    distance[start] = 0
-    
-    while heap:
-        dist, curr_node = heapq.heappop(heap)
-        if distance[curr_node] < dist:
+    dijkstra(K)
+
+    for i in range(1,V+1):
+        if i == K:
+            print(0)
             continue
-        for i in graph[curr_node]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(heap, (cost, i[0]))
-        
-dijkstra(K)
-
-for i in range(1, V+1):
-    if distance[i] == INF:
-        print("INF")
-    else:
+        if distance[i] == INF:
+            print("INF")
+            continue
         print(distance[i])
+
+
+if __name__ == '__main__':
+    solution()
