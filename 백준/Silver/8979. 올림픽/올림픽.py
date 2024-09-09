78ce09641727808b02cbb2c08923409e6b263550
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-09-09 12:32:46 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,29 @@
 import sys
 
+
 def solution():
     input = sys.stdin.readline
     N, K = map(int, input().split())
     country = []
     for _ in range(N):
         c, g, s, m = map(int, input().split())
-        country.append([c,g,s,m])
+        country.append([c, g, s, m])
     country.sort(key=lambda x: (-x[1], -x[2], -x[3]))
-
     rank = 1
-    for i in range(len(country)-1):
+    for i in range(len(country)):
+        if i == 0:
+            if country[i][0] == K:
+                print(1)
+                break
+            else:
+                continue
+        for j in range(1, 4):
+            if country[i-1][j] != country[i][j]:
+                rank = i+1
+                break
         if country[i][0] == K:
             print(rank)
             break
-        for j in range(1,4):
-            if country[i][j] != country[i+1][j]:
-                rank = i+2
 
 
 if __name__ == '__main__':
