6b64708b8e0b05e8627db99a092858d40a4181dc
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-09-17 20:19:34 +0900

    [Silver IV] Title: 카드2, Time: 116 ms, Memory: 50968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,17 @@
 from collections import deque
-n = int(input())
 
-q = deque()
-for i in range(1, n+1):
-    q.append(i)
-for _ in range(n-1):
-    q.popleft()
-    num = q.popleft()
-    q.append(num)
-print(*q)
+def solution():
+    n = int(input())
+
+    q = deque()
+    for i in range(1, n+1):
+        q.append(i)
+    for _ in range(n-1):
+        q.popleft()
+        num = q.popleft()
+        q.append(num)
+    print(*q)
+    
+if __name__ == '__main__':
+    solution()
+    
