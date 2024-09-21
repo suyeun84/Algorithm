be86bddd5a70f26fb04ff1ca94fdf6caa186c337
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-09-21 22:26:46 +0900

    [Silver II] Title: 비슷한 단어, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,24 +1,24 @@
 import sys
-import copy
-
 
 def solution():
     input = sys.stdin.readline
     N = int(input())
-    result = 0
-    target = list(map(str, input().strip()))
+    arr = [0]*26
+    target = list(input().strip())
+    answer = 0
+    for s in target:
+        arr[ord(s)-65] += 1
     for i in range(N-1):
-        check = list(map(str, input().strip()))
-        target2 = copy.deepcopy(target)
-        check2 = copy.deepcopy(check)
-        for j in target:
-            if j in check:
-                check.remove(j)
-                target2.remove(j)
-        if len(check) <= 1 and len(target2) <= 1:
-            result += 1
-        
-    print(result)
+        alpa = [0]*26
+        cnt = 0
+        word = list(input().strip())
+        for s in word:
+            alpa[ord(s)-65] += 1
+        for j in range(26):
+            cnt += abs(arr[j]-alpa[j])
+        if cnt <= 1 or (len(word) == len(target) and cnt == 2):
+            answer += 1
+    print(answer)
 
 
 if __name__ == '__main__':
