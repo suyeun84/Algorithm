54108cce28d1541202bd6e093cb7cb65a0e9dd0e
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-09-23 21:13:41 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -6,20 +6,12 @@ def solution():
     room = []
     for _ in range(p):
         l, n = map(str, input().strip().split(' '))
-        flag = False
-        if len(room) == 0:
-            room.append([[l, n]])
-            continue
+        flag = True
         for i in range(len(room)):
-            if len(room[i]) == m:
-                flag = True
-                continue
-            if abs(int(room[i][0][0]) - int(l)) <= 10:
+            if len(room[i]) < m and abs(int(room[i][0][0]) - int(l)) <= 10:
                 room[i].append([l, n])
                 flag = False
                 break
-            else:
-                flag = True
         if flag:
             room.append([[l, n]])
 
@@ -27,12 +19,10 @@ def solution():
         room[i].sort(key=lambda x: x[1])
         if len(room[i]) == m:
             print("Started!")
-            for j in range(m):
-                print(room[i][j][0]+' '+room[i][j][1])
         else:
             print("Waiting!")
-            for j in range(len(room[i])):
-                print(room[i][j][0]+' '+room[i][j][1])
+        for j in range(len(room[i])):
+            print(room[i][j][0]+' '+room[i][j][1])
 
 
 if __name__ == '__main__':
