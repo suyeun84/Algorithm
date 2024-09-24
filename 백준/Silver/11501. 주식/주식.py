918dfe33fba47005798251276368e9110c844e42
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-09-24 16:22:48 +0900

    [Silver II] Title: 주식, Time: 2068 ms, Memory: 176196 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -3,30 +3,18 @@ import sys
 def solution():
     input = sys.stdin.readline
     T = int(input())
+
     for _ in range(T):
         N = int(input())
-        curr = 0
-        answer = 0
-        check = [0] * N
-        price = list(map(int, input().split(' ')))
-        sorted_price = []
-        for i in range(N):
-            sorted_price.append([price[i], i])
-        sorted_price = sorted(sorted_price, key=lambda x:-x[0])
-
-        for i in range(N):
-            p = sorted_price[i][0]
-            idx = sorted_price[i][1]
-            if idx > curr:
-                for j in range(curr, idx):
-                    if check[j] == 0:
-                        answer += (p-price[j])
-                        check[j] = 1
-                curr = idx
-            check[idx] = 1
-            if curr == N-1:
-                break
-        print(answer)
+        price = list(map(int, input().split()))
+        money = 0
+        maxPrice = 0
+        for i in range(len(price)-1, -1, -1):
+            if price[i] > maxPrice:
+                maxPrice = price[i]
+            else:
+                money += maxPrice - price[i]
+        print(money)
 
 
 if __name__ == '__main__':
