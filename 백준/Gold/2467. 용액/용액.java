5c8337661e44c18b01ded857efceb81cd59e5b97
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-10-06 19:15:18 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28768 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -4,29 +4,25 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
 
-        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[n];
+        int start = 0, end = n-1;
+        int[] answer = new int[2];
+        int min_value = Integer.MAX_VALUE;
+        
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int start = 0;
-        int end = n-1;
-        int answer1 = 0;
-        int answer2 = 0;
-        int min_value = Integer.MAX_VALUE;
-
         while (start < end) {
-            int temp = list.get(start) + list.get(end);
+            int temp = arr[start] + arr[end];
             if (Math.abs(temp) < min_value) {
                 min_value = Math.abs(temp);
-                answer1 = list.get(start);
-                answer2 = list.get(end);
+                answer[0] = arr[start];
+                answer[1] = arr[end];
             }
             if (temp < 0) {
                 start += 1;
@@ -36,7 +32,6 @@ public class Main {
                 break;
             }
         }
-        sb.append(answer1 + " " + answer2);
-        System.out.println(sb.toString());
+        System.out.println(answer[0] + " " + answer[1]);
     }
 }
