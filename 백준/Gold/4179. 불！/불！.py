6efa5a155fdaa7943ad458f9edd8427eccfd2374
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2024-11-07 14:23:32 +0900

    [Gold III] Title: 불!, Time: 1172 ms, Memory: 66392 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -3,79 +3,66 @@ from collections import deque
 
 def solution():
     input = sys.stdin.readline
-    d = [[1,0], [-1,0], [0,1], [0,-1]]
-    board = []
-    q = deque()
-    fire = deque()
-    answer = 0
-
     R, C = map(int, input().split())
-    visited = [[0 for _ in range(C)] for _ in range(R)]
-    fire_cnt = [[int(1e9) for _ in range(C)] for _ in range(R)]
-
-
-    curr_J = [0,0]
-
+    board = []
+    d = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    jx, jy = 0, 0
+    fire = [[1e9 for _ in range(C)] for _ in range(R)]
+    point = []
+    answer = 1e9
     for _ in range(R):
         board.append(input().strip())
 
     for i in range(R):
         for j in range(C):
-            if board[i][j] == 'J':
-                q.append([i,j,1])
-                visited[i][j] = 1
-            elif board[i][j] == 'F':
-                fire.append([i,j,1])
-                fire_cnt[i][j] = 1
-            elif board[i][j] == '#':
-                visited[i][j] = 1
-    
-    def init_fire():
-        nonlocal fire
-        nonlocal fire_cnt
-        
-        while fire:
-            # fire
-            y, x, cnt = fire.popleft()
-            for dy, dx in d:
-                ny = y+dy
-                nx = x+dx
-                if 0>ny or ny>=R or 0>nx or nx>=C:
-                    continue
-                if board[ny][nx] == '#' or fire_cnt[ny][nx] <= cnt+1:
-                    continue
-                fire_cnt[ny][nx] = cnt+1
-                fire.append([ny, nx, cnt+1])
-            
-    def get_out():
-        nonlocal q
-        nonlocal visited
-        nonlocal fire_cnt
-        nonlocal answer
-        nonlocal board
+            if board[i][j] == "F":
+                point.append([i, j])
+            if board[i][j] == "J":
+                jx, jy = j, i
 
+    def init_fire():
+        nonlocal fire, point
+        q = deque()
+        for fy, fx in point:
+            q.append([fy, fx])
+            fire[fy][fx] = 1
         while q:
-            y, x, cnt = q.popleft()
-            if y==0 or y==R-1 or x==0 or x==C-1:
-                answer = cnt
+            y, x = q.popleft()
+            for dy, dx in d:
+                ny, nx = y + dy, x + dx
+                if 0 > ny or ny >= R or nx < 0 or nx >= C:
+                    continue
+                if board[ny][nx] == "#" or fire[ny][nx] != 1e9:
+                    continue
+                fire[ny][nx] = fire[y][x] + 1
+                q.append([ny, nx])
+
+    def bfs():
+        nonlocal answer
+        visited = [[-1 for _ in range(C)] for _ in range(R)]
+        q = deque()
+        q.append([jy, jx])
+        visited[jy][jx] = 1
+        while q:
+            y, x = q.popleft()
+            if y == 0 or x == 0 or y == R-1 or x == C-1:
+                answer = visited[y][x]
                 return True
             for dy, dx in d:
-                ny = y+dy
-                nx = x+dx
-                if 0>ny or ny>=R or 0>nx or nx>=C:
+                ny, nx = y + dy, x + dx
+                if 0 > ny or ny >= R or nx < 0 or nx >= C:
                     continue
-                if board[ny][nx] == '#' or visited[ny][nx] == 1 or fire_cnt[ny][nx] <= cnt+1:
+                if board[ny][nx] == "#" or visited[ny][nx] != -1 or (fire[ny][nx] <= visited[y][x]+1):
                     continue
-                visited[ny][nx] = 1
-                q.append([ny, nx, cnt+1])
+                visited[ny][nx] = visited[y][x] + 1
+                q.append([ny, nx])
         return False
 
     init_fire()
-
-    if get_out():
+    if bfs():
         print(answer)
     else:
-        print('IMPOSSIBLE')
+        print("IMPOSSIBLE")
 
 
 if __name__ == '__main__':
